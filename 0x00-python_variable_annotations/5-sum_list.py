3903d8c11f4b61c09f20f9b0e2955f0070f2872c
Uduak Umanah <eyakenojnr@gmail.com>
2024-11-07 18:03:48 +0100

    Reverted to previous code version

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 '''Complex types - list of floats.
 '''
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''Computes the sum of a list of floating-point numbers.
     '''
     return sum(input_list)
